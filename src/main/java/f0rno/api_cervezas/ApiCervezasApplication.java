fd90be2f887eeb242ac2e45071ec181b7e98c40e
F0rno <pablo.fornell.perinan@gmail.com>
2023-10-27 09:10:25 +0200

    Initial commit

---
+++ src/main/java/f0rno/api_cervezas/ApiCervezasApplication.java
@@ -0,0 +1,13 @@
+package f0rno.api_cervezas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCervezasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCervezasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f0rno/api_cervezas/ApiCervezasApplicationTests.java
@@ -0,0 +1,13 @@
+package f0rno.api_cervezas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCervezasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
