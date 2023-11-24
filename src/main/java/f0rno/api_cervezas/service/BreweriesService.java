3d74ba829daab1691c21a3b50d3c9e0801bc5808
F0rno <pablo.fornell.perinan@gmail.com>
2023-11-24 08:28:49 +0100

    [DONE] Breweries exceptions

--- src/main/java/f0rno/api_cervezas/service/BreweriesService.java
+++ src/main/java/f0rno/api_cervezas/service/BreweriesService.java
@@ -1,10 +1,12 @@
 package f0rno.api_cervezas.service;
 
+import f0rno.api_cervezas.error.brewery.BreweryNotFoundException;
 import f0rno.api_cervezas.model.Breweries;
 import f0rno.api_cervezas.repository.BreweriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,7 +21,9 @@ public class BreweriesService {
     public Page<Breweries> getAllBreweries(int page, int size) {
         return breweriesRepository.findAll(PageRequest.of(page, size));
     }
-    public Breweries getBrewery(int id) {
-        return breweriesRepository.findById(id).orElse(null);
+    public ResponseEntity<Breweries> getBrewery(int id) throws BreweryNotFoundException {
+        return breweriesRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new BreweryNotFoundException(id));
     }
 }
