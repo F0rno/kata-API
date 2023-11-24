1bc8a3a61586d703040aa6ba60823337b49b6ab1
F0rno <pablo.fornell.perinan@gmail.com>
2023-11-24 08:30:26 +0100

    [DONE] Categories exceptions

--- src/main/java/f0rno/api_cervezas/controller/CategoriesController.java
+++ src/main/java/f0rno/api_cervezas/controller/CategoriesController.java
@@ -1,8 +1,10 @@
 package f0rno.api_cervezas.controller;
 
+import f0rno.api_cervezas.error.categorie.CategorieNotFoundExecption;
 import f0rno.api_cervezas.model.Categories;
 import f0rno.api_cervezas.service.CategoriesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +23,7 @@ public class CategoriesController {
         return categoriesService.getAllCategories();
     }
     @GetMapping("/categories/{id}")
-    public Categories getCategory(@PathVariable int id) {
+    public ResponseEntity<Categories> getCategory(@PathVariable int id) throws CategorieNotFoundExecption {
         return categoriesService.getCategory(id);
     }
 }
--- src/main/java/f0rno/api_cervezas/service/CategoriesService.java
+++ src/main/java/f0rno/api_cervezas/service/CategoriesService.java
@@ -1,8 +1,10 @@
 package f0rno.api_cervezas.service;
 
+import f0rno.api_cervezas.error.categorie.CategorieNotFoundExecption;
 import f0rno.api_cervezas.model.Categories;
 import f0rno.api_cervezas.repository.CategoriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +18,9 @@ public class CategoriesService {
     public List<Categories> getAllCategories() {
         return categoriesRepository.findAll();
     }
-    public Categories getCategory(int id) {
-        return categoriesRepository.findById(id).orElse(null);
+    public ResponseEntity<Categories> getCategory(int id) throws CategorieNotFoundExecption {
+        return categoriesRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElseThrow(() -> new CategorieNotFoundExecption(id));
     }
 }
